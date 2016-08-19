8a978bc1a7b520bbccd98b86c918a526d8524ba0
nir@500tech.com <nir@500tech.com>
2016-08-19 17:40:48 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app";
+import {BrowserModule} from "@angular/platform-browser";
+import {HttpModule} from "@angular/http";
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports     : [BrowserModule, HttpModule,],
+  providers   : [{
+    provide : LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
+  bootstrap   : [AppComponent]
+})
+
+export class AppModule {}
---
+++ src/app/app.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector   : 'app',
+  template: `<h1> Angular 2 Seed </h1>`,
+})
+
+export class AppComponent {}
