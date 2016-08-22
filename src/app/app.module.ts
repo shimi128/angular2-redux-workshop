1c869696a0c3b7884f5c82e0c17239d9a47f1334
nir@500tech.com <nir@500tech.com>
2016-08-22 13:23:50 +0300

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from "@angular/core";
-import {AppComponent} from "./app";
+import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {HttpModule} from "@angular/http";
 import {LocationStrategy, HashLocationStrategy} from "@angular/common";
